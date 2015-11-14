f6f82b6f07ff5b374958acb45ffd8e0b2d2154ad
Admin <admin@mac-admin.local>
2015-11-14 12:59:41 +0300

    Initial Commit

---
+++ simplegram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  simplegram
+//
+//  Created by Admin on 14.11.15.
+//  Copyright © 2015 Manshilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
